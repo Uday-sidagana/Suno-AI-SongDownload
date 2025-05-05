7cb91c17b22ada48d9f180cd982da625f1103ab3
Uday-sidagana <uday.sidgana@gmail.com>
2025-05-06 00:44:07 +0530

    Working on the Flask webpage

--- suno.py
+++ suno.py
@@ -8,5 +8,10 @@ app = Flask(__name__, template_folder="Template")
 def hello():
     return "Hello"
 
+
+@app.route('/homepage')
+def homepage():
+    pass
+
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5020, debug=True)
