19d140c6d9f778cd3eac9112177b367d901fcbb5
Uday-sidagana <uday.sidgana@gmail.com>
2025-05-05 21:00:09 +0530

    Base for Flask

---
+++ suno.py
@@ -0,0 +1,12 @@
+from flask import Flask, Request, render_template, redirect, url_for
+
+
+
+app = Flask(__name__, template_folder="Template")
+
+@app.route('/')
+def hello():
+    return "Hello"
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5020, debug=True)
